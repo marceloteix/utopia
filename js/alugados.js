002643f78ffa12d7de908235f2bd7daaa756e1ca
marceloteix <103069438+marceloteix@users.noreply.github.com>
2024-07-19 19:27:46 -0300

    alugados.js

--- js/alugados.js
+++ js/alugados.js
@@ -1,31 +1,33 @@
 // Função para renderizar itens emprestados
 function renderizarItensEmprestados() {
-    // Recuperar a lista de itens emprestados do localStorage
-    var itensEmprestados = JSON.parse(localStorage.getItem('itensEmprestados')) || [];
+    setTimeout(function() {
+        // Recuperar a lista de itens emprestados do localStorage
+        var itensEmprestados = JSON.parse(localStorage.getItem('itensEmprestados')) || [];
 
-    // Verificar se há itens emprestados
-    if (itensEmprestados.length > 0) {
-        // Percorrer os itens emprestados e renderizá-los
-        $.each(itensEmprestados, function (index, item) {
-            var itemDiv = `
-            <li class="item-content">
-                <div class="item-media">
-                    <img src="${item.imagem}" width="120">
-                </div>
-                <div class="item-inner">
-                    <div class="item-title">${item.nome}</div>
-                    <div class="item-subtitle">Emprestado por: ${item.nomeCompleto}</div>
-                    <div class="item-text">Até: ${item.data}</div>
-                </div>
-            </li>
-            `;
-            // Adicionar o item à lista de itens emprestados
-            $("#alugados-list").append(itemDiv);
-        });
-    } else {
-        // Mostrar mensagem de que não há itens emprestados
-        $("#alugados-list").html('<li class="item-content"><div class="item-inner"><div class="item-title">Nenhum item emprestado.</div></div></li>');
-    }
+        // Verificar se há itens emprestados
+        if (itensEmprestados.length > 0) {
+            // Percorrer os itens emprestados e renderizá-los
+            $.each(itensEmprestados, function (index, item) {
+                var itemDiv = `
+                <li class="item-content">
+                    <div class="item-media">
+                        <img src="${item.imagem}" width="120">
+                    </div>
+                    <div class="item-inner">
+                        <div class="item-title">${item.nome}</div>
+                        <div class="item-subtitle">Emprestado por: ${item.nomeCompleto}</div>
+                        <div class="item-text">Até: ${item.data}</div>
+                    </div>
+                </li>
+                `;
+                // Adicionar o item à lista de itens emprestados
+                $("#alugados-list").append(itemDiv);
+            });
+        } else {
+            // Mostrar mensagem de que não há itens emprestados
+            $("#alugados-list").html('<li class="item-content"><div class="item-inner"><div class="item-title">Nenhum item emprestado.</div></div></li>');
+        }
+    }, 500); // Adiciona um atraso de 500 milissegundos antes de executar a função
 }
 
 // Chama a função para renderizar itens emprestados quando o documento estiver pronto
